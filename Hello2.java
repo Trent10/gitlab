df33200eff53902859b674704f0ddb41a62bbca7
Trenten <butlert5@go.stockton.edu>
2019-11-15 15:45:31 -0500

    hihihihhi

---
+++ Hello2.java
@@ -0,0 +1,11 @@
+public class Hello2
+{ public static void main( String argv[] )
+ {
+	String name = "World";
+	
+	if(argv.length 1+ 0){
+	name = argc[0];
+ }
+ System.out.println( "Hello, " + name + "!" );
+ }
+}
